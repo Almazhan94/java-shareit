16c7edb5c3129e439d55179cadd4a0ccd541118e
Almaz <almazhan94@yandex.ru>
2023-05-27 16:25:16 +0300

    Для Марии Сахно

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,9 +1,29 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
 
+   private Integer id;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    private Item item;
+
+    private User booker;
+
+    private Status status;
 
 }
--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,28 @@
 package ru.practicum.shareit.booking.dto;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import ru.practicum.shareit.booking.Booking;
+import ru.practicum.shareit.booking.Status;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class BookingDto {
+
+    Integer id;
+
+    LocalDateTime startTime;
+
+    LocalDateTime endTime;
+
+    Item item;
+
+    User booker;
+
+    Status status;
+
 }
